83caaaca81fefa917a94eadb9921b4420c675a47
Vothanh2803 <vothanh28032007@gmail.com>
2026-05-14 19:51:00 +0700

    add_TKU

--- Assets/Scripts/Minigame/Clicker/PuchGameController.cs
+++ Assets/Scripts/Minigame/Clicker/PuchGameController.cs
@@ -7,6 +7,7 @@ public class PunchGameController : MinigameController
 {
     [Header("UI")]
     public Image mainDisplayImage; 
+    public Image actionDisplayImage;
     public Image forceBarFill;
 
     private PunchMinigameSO gameData;
@@ -15,6 +16,7 @@ public class PunchGameController : MinigameController
     private float currentForce = 0f;
     private Tweener forceTweener;
     private bool isWaitingForEnd = false;
+    private Coroutine punchRoutine;
 
     protected override void OnInit()
     {
@@ -32,6 +34,12 @@ public class PunchGameController : MinigameController
         currentForce = 0f;
         isWaitingForEnd = false;
         forceBarFill.fillAmount = 0f;
+
+        if (actionDisplayImage != null && gameData != null && gameData.ActionStanceSprite != null)
+        {
+            actionDisplayImage.sprite = gameData.ActionStanceSprite;
+        }
+
         UpdateCurrentLevel();
     }
 
@@ -60,12 +68,33 @@ public class PunchGameController : MinigameController
         if (forceTweener != null) forceTweener.Kill();
         forceTweener = forceBarFill.DOFillAmount(currentForce, 0.05f).SetEase(Ease.OutCubic);
 
+        PlayActionAnimation();
 
         UpdateCurrentLevel();
 
         if (currentForce >= 1f)
         {
             StartCoroutine(WinSequenceRoutine());
+        }
+    }
+
+    private void PlayActionAnimation()
+    {
+        if (actionDisplayImage != null && gameData != null && gameData.ActionPunchSprite != null)
+        {
+            actionDisplayImage.sprite = gameData.ActionPunchSprite;
+
+            if (punchRoutine != null) StopCoroutine(punchRoutine);
+            punchRoutine = StartCoroutine(ResetStanceRoutine());
+        }
+    }
+
+    private IEnumerator ResetStanceRoutine()
+    {
+        yield return new WaitForSeconds(gameData.ActionPunchDuration);
+        if (actionDisplayImage != null && gameData.ActionStanceSprite != null)
+        {
+            actionDisplayImage.sprite = gameData.ActionStanceSprite;
         }
     }
 
@@ -76,7 +105,10 @@ public class PunchGameController : MinigameController
             if (currentForce >= gameData.LevelConfigs[i].ForceThreshold)
             {
                 currentLevelConfig = gameData.LevelConfigs[i];
-                mainDisplayImage.sprite = currentLevelConfig.LevelSprite;
+                if (mainDisplayImage != null && currentLevelConfig.LevelSprite != null)
+                {
+                    mainDisplayImage.sprite = currentLevelConfig.LevelSprite;
+                }
                 break;
             }
         }
@@ -89,11 +121,9 @@ public class PunchGameController : MinigameController
         if (forceTweener != null) forceTweener.Kill();
         forceBarFill.fillAmount = 1f;
 
-
         yield return new WaitForSeconds(2f);
         WinGame();
     }
-
 
     public new void Btn_Retry()
     {
